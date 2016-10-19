38686bee6819aa690549292ad1b005a2b4822fca
jesse <jesseaborden@gmail.com>
2016-10-19 13:48:07 -0700

    [feature]-started adding state to room parent component. get working

--- src/components/Room/map_component.js
+++ src/components/Room/map_component.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Gmap = props => (
-  <iframe
-    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBlnC4Cp2iBrYCbpQ4jLEYIhD4_MGQXxd4&q=Space+Needle,Seattle+WA" className="center-block">;
-  </iframe>
+<div>
+	<iframe className="center-block" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBlnC4Cp2iBrYCbpQ4jLEYIhD4_MGQXxd4&q=Space+Needle,Seattle+WA"></iframe>
+		<button className="center-block">end game</button>
+		<button className="center-block">leave game</button>
+ </div>
 );
 
 export default Gmap;

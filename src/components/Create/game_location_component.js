1b280d244c23f12c9a13eee39abe1b997579e6a5
Tim <timotheeaneumann@gmail.com>
2016-10-18 20:51:18 -0700

    [add]-added static version of creat game page

---
+++ src/components/Create/game_location_component.js
@@ -0,0 +1,12 @@
+//Libs
+import React, { Component } from 'react';
+
+class GameLocation extends Component {
+	render () {
+		return (
+			<input type='text' name='location' placeholder='location' />
+		);
+	}
+}
+
+export default GameLocation;

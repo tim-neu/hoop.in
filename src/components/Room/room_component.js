7010b3d16f5b0eb9fc8b4c298c713c244ea8d411
jesse <jesseaborden@gmail.com>
2016-10-20 20:18:08 -0700

    [add]-added comments to room

--- src/components/Room/room_component.js
+++ src/components/Room/room_component.js
@@ -2,29 +2,33 @@ import React, { Component } from 'react';
 import Gmap from './map_component.js';
 import Queue from './queue_component.js';
 import axios from 'axios';
-let token = "bbbfj";
 
 class Room extends React.Component {
   constructor (props) {
     super(props),
-    this.state = {};
+    this.state = {
+      teams: { redteam: null, blueteam: null },
+    };
   }
 
-  // componentWillMount() {
-  //   this.getTeam();
-  // }
-  
-  // getTeam() {
-  //   const teamGetCall = () => (axios.get(`/api/getPlayers?token=${token}`)
-  //   .then(function (response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   }));
-  //   teamGetCall();
-  //   setInterval(teamGetCall, 5000);
-  // }
+  componentWillMount() {
+    this.getTeam();
+  }
+
+  getTeam() {
+    const teamGetCall = () => (axios.get(`/api/getPlayers?token=${token}`)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    );
+    teamGetCall();
+    setInterval(teamGetCall, 5000);
+  }
+
+  // pass teams object into queue
 
   render () {
     return (

206d96f4cc6f299dbc53ae972dd8de39734863c0
jesse <jesseaborden@gmail.com>
2016-10-21 17:02:39 -0700

    rendering team members on screen

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -5,65 +5,95 @@ var playerController = {};
 
 //FILL OUT YOUR CODE JESSE
 playerController.GET = (req, res) => {
-  res.send('i am in player controller!');
-};
+  //find game pluck id from game
+  var token = req.query.token;
+  var playerData = { team1: [], team2: [], queue: [] };
+  Game.findOne({ where: {token: token}})
+  .then(function(game){
+    Team.findAll({ where: {gameId: game.id}})
+    .then(function(teams){
+        var team1 = teams[0];
+        var team2 = teams[1];
+        Player.findAll({ where: {teamId: team1.id}})
+        .then(function(players1){
+            playerData.team1 = players1;
+        });
+        Player.findAll({ where: {teamId: team2.id}})
+        .then(function(players2){
+            playerData.team2 = players2;
+        });
+        Player.findAll({ where: {teamId: null}})
+        .then(function(queue){
+            playerData.queue = queue;
+            res.send(playerData);
+        });
+    }).catch(function(){
+        console.log("shit went left with the team get inside the game get")
+    });
+  }).catch(function(err){
+    console.log("shit went left with the game get")
+  });
+
+  }
+
+  //find teams with matching game id grab team id
+  // create empty object
+  // save < team id to prop team1
+  // save > team id to prop team2
+  // save players with game id but no teamid to queue
+
+
 
 playerController.POST = (req, res) => {
-	//res.send("I'm in player controller post correctly!");
-	var requestToken = '123'; //get from req object, hard coding now
-	var newPlayer = Player.build({
-		arrivalTime: '9PM',
-		active: false,
-		queued: true,
-		name: req.body.name,
-		admin: false,
-	});
-	//should be taken form the req object hard code now
-	Game.findOne({where: {token: requestToken}}).then(function(game){
-		console.log('I found the game!',game);
-		console.log('game id is:',game.dataValues.id);
-		Team.findAll({where: {gameId: game.dataValues.id}}).then(function(teams){
-			console.log('i found the teams with the right game ID!',teams);
-			var team1 = teams[0];
-			var curCount1 = team1.dataValues.count;
-			var team2 = teams[1];
-			var curCount2 = team2.dataValues.count;
-			if (curCount1 < 3){
-				newPlayer.save().then(function(){
-					console.log(' team1 has a spot open');
-					this.setTeam(team1);
-					this.setGame(game);
-				})
-				curCount1++;
-				team1.update({count: curCount1});
-			}
-			else if (curCount2 < 3){
-				newPlayer.save().then(function(){
-					console.log(' team 2 has a spot open');
-					this.setTeam(team2);
-					this.setGame(game);
-				})
-				curCount2++;
-				team2.update({count: curCount2});
-			}
-			else{
-				newPlayer.save().then(function(){
-
-					console.log('the player has to wait for the next game');
-					console.log('check arrivalTime to see who the next player is');
-					this.setGame(game);
-				})
-			}
-
-
-		})
-
-	}).catch(function(err){
-		console.log('i couldnt find the game');
-	});
-
+    //res.send("I'm in player controller post correctly!");
+    var requestToken = req.body.token; //get from req object, hard coding now
+    var newPlayer = Player.build({
+        arrivalTime: '9PM',
+        active: false,
+        queued: true,
+        name: req.body.name,
+        admin: false,
+    });
+    //should be taken form the req object hard code now
+    Game.findOne({ where: {token: requestToken}}).then(function(game){
+        console.log('I found the game!',game);
+        console.log('game id is:',game.dataValues.id);
+        Team.findAll({where: {gameId: game.dataValues.id}}).then(function(teams){
+            console.log('i found the teams with the right game ID!',teams);
+            var team1 = teams[0];
+            var curCount1 = team1.dataValues.count;
+            var team2 = teams[1];
+            var curCount2 = team2.dataValues.count;
+            if (curCount1 <= 4){
+                newPlayer.save().then(function(){
+                    console.log(' team1 has a spot open');
+                    this.setTeam(team1);
+                    this.setGame(game);
+                })
+                curCount1++;
+                team1.update({count: curCount1});
+            }
+            else if (curCount2 <= 4){
+                newPlayer.save().then(function(){
+                    console.log(' team 2 has a spot open');
+                    this.setTeam(team2);
+                    this.setGame(game);
+                })
+                curCount2++;
+                team2.update({count: curCount2});
+            }
+            else{
+                newPlayer.save().then(function(){
+                    console.log('the player has to wait for the next game');
+                    console.log('check arrivalTime to see who the next player is');
+                    this.setGame(game);
+                })
+            }
+        })
+    }).catch(function(err){
+        console.log('i couldnt find the game');
+    });
     res.send('/api/newPlayer');
-
 };
 
 
--- src/components/Share/share_component.js
+++ src/components/Share/share_component.js
@@ -6,9 +6,13 @@ import axios from 'axios';
 
 class Share extends Component {
   constructor (props) {
-    super(props)
+    super(props),
 
     this.state = {};
+  };
+
+  componentDidMount() {
+    this.setState({ token: localStorage.getItem('token') });
   }
 
   //GET token to form share link
@@ -17,9 +21,9 @@ class Share extends Component {
     return (
       <div className='text-center'>
         <section><h1>Share Game</h1></section>
-        <div>https://hoopi.in/Link-to-room</div>
+        <div>{`https://hoop.in/room/${this.state.token}`}</div>
         <div>
-          <button><Link to='/room/ponys' >create</Link></button>
+          <button><Link to={`room/${this.state.token}`} >create</Link></button>
           <button><Link to='/create' >back</Link></button>
         </div>
       </div>

e9554fc3fb7743bb86536ed53bbd9d01a85e8e19
jesse <jesseaborden@gmail.com>
2016-10-21 19:19:14 -0700

    [fix]-worked on fixing the bugs on the room page and the flow up to it

--- src/components/Room/queue_component.js
+++ src/components/Room/queue_component.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Team from './team_component.js';
 
 const Queue = props => {
-  if(props.players.team1.length === 0 && props.players.team2.length === 0){
-  	{console.log(props.players, "inside Queue first if")}
+  if(props.players.team1.length === 0 && props.players.team2.length === 0) { 
+  	console.log(props.players, "inside Queue first if")
 	return (<h1 className="text-center">Loading</h1>)
-  }
+  } 
   else{
+  	console.log(props.players.team1,"props.players.team1 in else")
 	return (<div>
-		{console.log(props.players.team1,"props.players.team1 in else")}
 		<Team team={props.players.team1} color={'blue'}/>
 	  	<Team team={props.players.team2} color={'red'}/>
 	  </div>)

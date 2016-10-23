9339907bf3504c3f45023687a5d2ce3b2a5bb7a2
Tim <timotheeaneumann@gmail.com>
2016-10-23 16:19:16 -0700

    [add]-added webpack config for css modules, started styling home and create page

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://eghnhvmy:4Ux6rwaZSerOqXvyNIX6sgnfVfftpz4B@elmer.db.elephantsql.com:5432/eghnhvmy');
+var sequelize = new Sequelize('postgres://svmuoadp:8ZWQdJbithVIP05btL4TQVtIn-_Y3FtV@elmer.db.elephantsql.com:5432/svmuoadp');
 
 module.exports = {
   sequelize: sequelize,
